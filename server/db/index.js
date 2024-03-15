e1d5bf6fe46f6c7b25747537df6596f7ab4dc1d9
Rıza BAT <rizabat09@gmail.com>
2024-03-15 17:00:07 +0300

    Update index.js

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 mongoose
   .connect(
-    "mongodb+srv://rizabat09:fapyUtrzO3gBIXlB@cluster0.xucbv5s.mongodb.net/"
+    "mongodb+srv://*******:********************.********.mongodb.net/"
   )
   .then(() => console.log("Connected mongo db"))
   .catch((e) => console.log(e));
